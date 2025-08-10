d58cd2e6fa98eeda2c7e96cc0a13a29638ccae5d
sun-guannan <sguann@qq.com>
2025-08-10 10:39:24 +0800

    初始化CapCutMaker项目

---
+++ main.js
@@ -0,0 +1,98 @@
+const { app, BrowserWindow, protocol } = require('electron');
+const path = require('path');
+const url = require('url');
+
+// 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
+// 垃圾回收的时候，window对象将会自动的关闭
+let mainWindow;
+
+function createWindow() {
+  // 创建浏览器窗口
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // 加载index.html文件
+  mainWindow.loadFile('index.html');
+
+  // 打开开发者工具
+  // mainWindow.webContents.openDevTools();
+
+  // 当window被关闭，这个事件会被触发
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+// 注册自定义协议
+app.whenReady().then(() => {
+  protocol.registerFileProtocol('capcuthelper', (request, callback) => {
+    const url = request.url.substr('capcuthelper://'.length);
+    try {
+      return callback(decodeURIComponent(url));
+    } catch (error) {
+      console.error('Failed to register protocol', error);
+    }
+  });
+});
+
+// 处理协议启动
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // 当运行第二个实例时，将会聚焦到mainWindow这个窗口
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+      
+      // 处理协议URL
+      const url = commandLine.pop();
+      if (url.startsWith('capcuthelper://')) {
+        // 在这里处理URL参数
+        console.log('Protocol URL:', url);
+        // 可以将URL参数发送到渲染进程
+        if (mainWindow.webContents) {
+          mainWindow.webContents.send('protocol-url', url);
+        }
+      }
+    }
+  });
+
+  // Electron 完成初始化并准备创建浏览器窗口时调用此方法
+  app.whenReady().then(createWindow);
+
+  // 协议处理 - macOS
+  app.on('open-url', (event, url) => {
+    event.preventDefault();
+    if (url.startsWith('capcuthelper://')) {
+      console.log('Protocol URL (macOS):', url);
+      if (mainWindow && mainWindow.webContents) {
+        mainWindow.webContents.send('protocol-url', url);
+      }
+    }
+  });
+
+  // 当全部窗口关闭时退出
+  app.on('window-all-closed', function () {
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活
+    if (process.platform !== 'darwin') app.quit();
+  });
+
+  app.on('activate', function () {
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口
+    if (mainWindow === null) createWindow();
+  });
+}
+
+// 在macOS上，需要在app.setAsDefaultProtocolClient之前调用这个
+app.setAsDefaultProtocolClient('capcuthelper');
